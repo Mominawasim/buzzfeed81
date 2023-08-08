c3b6091460e6702ecacb2155d357d990501e082c
Mominawasim <141708925+Mominawasim@users.noreply.github.com>
2023-08-08 12:00:52 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://wbari129.systeme.io/510b24fe` + encodeURI(path as string)
+					`https://wbari129.systeme.io/510b24fe`
 				}`,
 			},
 		};
@@ -30,7 +30,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://wbari129.systeme.io/510b24fe` + encodeURI(path as string)
+					`https://wbari129.systeme.io/510b24fe`
 				}`,
 			},
 		};
