d12aff1e8210add1f381752af306f960c20583a4
Mominawasim <141708925+Mominawasim@users.noreply.github.com>
2023-08-09 00:38:06 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://buzzfeed81.000webhostapp.com/graphql"
+	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,18 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://wbari129.systeme.io/510b24fe`
-				}`,
-			},
-		};
-		}
-		if (referringURL?.includes('pinterest.com') || pin) {
-
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					`https://wbari129.systeme.io/510b24fe`
+					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
 				}`,
 			},
 		};
